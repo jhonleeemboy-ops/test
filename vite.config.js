b8a1e825b5771f7de1180f0677631c3d36689609
jhonleeemboy-ops <jhonlee.emboy@carsu.edu.ph>
2026-02-14 01:48:09 +0800

    add dist build

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/Valentine-s/',
+  base: '/test/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
